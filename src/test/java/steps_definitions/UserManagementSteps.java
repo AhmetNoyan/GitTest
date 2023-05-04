eb57ddc94af241dd2e965b2d12cad924b27ac256
AhmetNoyan <ahmetnoyan44@gmail.com>
2023-05-04 16:13:48 -0400

    cucumber feature file steps created

---
+++ src/test/java/steps_definitions/UserManagementSteps.java
@@ -0,0 +1,28 @@
+package steps_definitions;
+
+import io.cucumber.java.en.*;
+
+public class UserManagementSteps {
+
+	
+	@Given("As a user, I am on the login page")
+	public void as_a_user_i_am_on_the_login_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@When("I enter valid username and valid password")
+	public void i_enter_valid_username_and_valid_password() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@When("I click on login button")
+	public void i_click_on_login_button() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@Then("I should be on user profile page")
+	public void i_should_be_on_user_profile_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+}
